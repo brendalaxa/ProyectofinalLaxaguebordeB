5d2355350b6d3ddc598a68f75d2e2dcc6db311dd
brendalaxa <123613825+brendalaxa@users.noreply.github.com>
2023-09-14 21:01:58 -0300

    Add files via upload

---
+++ ecommerce/src/components/CarWidget/CarWidget.jsx
@@ -0,0 +1,13 @@
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
+const CarWidget = () => {
+  return (
+    <>
+      {" "}
+      <AddShoppingCartIcon sx={{ color: "black" }} />{" "}
+      <span style={{ color: "black" }}>(7)</span>{" "}
+    </>
+  );
+};
+
+export default CarWidget;
